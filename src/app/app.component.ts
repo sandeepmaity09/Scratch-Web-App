d4b2952b83a114199de90e93f98644110fdcbe6a
sandeepmaity09 <sandeepmaity09@gmail.com>
2017-07-28 21:41:00 +0530

    component learned

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,19 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `<h1>Hello {{name}}</h1>`,
+    template: `
+        <div>Hello my name is {{name}}.
+            <button (click)="sayMyName()">Say my Name</button>
+         </div>`,
 })
 
-export class AppComponent  { name = 'World!'; }
+export class AppComponent  {
+
+    name:string;
+    constructor(){
+        this.name = "Sandeep Maity"
+    }
+    sayMyName(){
+        console.log("My Name is ",this.name)
+    }
+}
